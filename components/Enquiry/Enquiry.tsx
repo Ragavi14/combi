9f4a57816017429f29790fbe76807b205cab8afc
shashi <shashi09568@gmail.com>
2023-08-02 16:36:49 +0530

    changes on home page

--- components/Enquiry/Enquiry.tsx
+++ components/Enquiry/Enquiry.tsx
@@ -6,14 +6,14 @@ const Enquiry = () => {
     return (
         <div className={`${styles.Enquiry}`}>
             <div className='container'>
-                <div className="row">
+                <div className="row justify-content-center align-items-center">
                     <div className="col-md-6">
                         <p>A question? A project? Call us on </p>
                         <h6> +91 97427 85352 / 
                            <Link href="#">  mailto:sales@combifer.com </Link> </h6>
                     </div>
-                    <div className="col-md-6">
-                        <button>Enquiry</button>
+                    <div className="col-md-6 text-end pe-5">
+                        <button className={`btn btn-primary btn-lg `}>Enquiry</button>
                     </div>
                 </div>
             </div>
